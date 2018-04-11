fc0ced3a21f126526e128bf62dec87c484f62845
rshah02 <37441582+rshah02@users.noreply.github.com>
2018-04-11 11:54:21 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomerServiceFacilitation.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerServiceFacilitation.Startup))]
+namespace CustomerServiceFacilitation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
